dc700efc26de9e433de3ad16a1548d88566931cb
maduma <maduma@pt.lu>
2017-09-12 22:09:02 +0000

    split Group1 to Group1 and Group2. Fix python 3 bug with dict_keys

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
 import uuid
-from flask_login import LoginManager, login_required, UserMixin, login_manager
+from flask_login import LoginManager, login_required, UserMixin #, login_manager
 
 app = Flask(__name__)
 api = Api(app)
@@ -12,11 +12,14 @@ login_manager.init_app(app)
 token = None
 groups = {'admin': ['snsakala', 'ckoenig'], 'dxretail': ['ckoenig']}
 
+
 class User(UserMixin):
     def __init__(self, user_id):
         self.id = user_id
+
     def get_id(self):
         return id
+
 
 @login_manager.request_loader
 def load_user_from_request(request):
@@ -24,76 +27,81 @@ def load_user_from_request(request):
         return User('snsakala')
     return None
 
-class Group1(Resource):
-    def get(self, group_id=None):
-        if group_id:
-            if group_id in groups:
-                return { group_id: groups[group_id] }
-            return { 'message': 'cannot find group ' + group_id}, 404
-        return groups.keys()
-    
-    @login_required
-    def put(self, group_id=None):
-        if group_id:
-            groups[group_id] = []
-            return { 'message': group_id + ' created'}
-        return { 'message': 'Please provide a groupId'}, 400
 
-    @login_required
-    def delete(self, group_id=None):
-        if not group_id:
-            return { 'message': 'please provide group_id' }
-        if group_id in groups:
-            if len(groups[group_id]) == 0:
-                groups.pop(group_id)
-                return { 'message': 'group delete' }
-            return { 'message': 'group not empty' }, 400
-        return { 'message': 'group not existing'} 
+class Group1(Resource):
+    def get(self):
+        return list(groups.keys())
+
 
 class Group2(Resource):
     def get(self, group_id):
         if group_id in groups:
-            return groups[group_id] 
-        return { 'message': 'cannot find group ' + group_id}, 404
+            return {group_id: groups[group_id]}
+        return {'message': 'cannot find group ' + group_id}, 404
+
+    @login_required
+    def put(self, group_id):
+        groups[group_id] = []
+        return {'message': group_id + ' created'}
+
+    @login_required
+    def delete(self, group_id):
+        if group_id in groups:
+            if len(groups[group_id]) == 0:
+                groups.pop(group_id)
+                return {'message': 'group delete'}
+            return {'message': 'group not empty'}, 400
+        return {'message': 'group not existing'}
+
 
 class Group3(Resource):
+    def get(self, group_id):
+        if group_id in groups:
+            return groups[group_id]
+        return {'message': 'cannot find group ' + group_id}, 404
+
+
+class Group4(Resource):
     @login_required
     def put(self, group_id, user_id):
         if group_id in groups:
             if user_id not in groups[group_id]:
                 groups[group_id].append(user_id)
-                return groups[group_id] 
-            return { 'message': 'user already in group'}
-        return { 'message': 'cannot find group ' + group_id}, 404
+                return groups[group_id]
+            return {'message': 'user already in group'}
+        return {'message': 'cannot find group ' + group_id}, 404
 
     @login_required
     def delete(self, group_id, user_id):
         if group_id in groups:
             if user_id in groups[group_id]:
                 groups[group_id].remove(user_id)
-                return { 'message': 'user removed from group' } 
-            return { 'message': 'cannot find user ' + user_id}, 404
-        return { 'message': 'cannot find group ' + group_id}, 404
+                return {'message': 'user removed from group'}
+            return {'message': 'cannot find user' + user_id}, 404
+        return {'message': 'cannot find group' + group_id}, 404
+
 
 class Users1(Resource):
     def get(self):
         users = []
         for group_id in groups:
             for user_id in groups[group_id]:
-                if not user_id in users:
+                if user_id not in users:
                     users.append(user_id)
         return users
+
 
 class Users2(Resource):
     def get(self, user_id):
         users = []
         for group_id in groups:
             for user in groups[group_id]:
-                if not user in users:
+                if user not in users:
                     users.append(user)
         if user_id in users:
             return user_id
-        return { 'message': 'cannot find user' }, 404
+        return {'message': 'cannot find user'}, 404
+
 
 class Users3(Resource):
     def get(self, user_id):
@@ -102,6 +110,7 @@ class Users3(Resource):
             if user_id in groups[group_id]:
                 user_groups.append(group_id)
         return user_groups
+
 
 class Login(Resource):
 
@@ -117,15 +126,18 @@ class Login(Resource):
             return {'token': token}
         return {'message': 'access denied'}, 403
 
+
 class Logout(Resource):
     def get(self):
         global token
         token = None
-        return { 'message': 'logged out'}
+        return {'message': 'logged out'}
 
-api.add_resource(Group1, '/groups', '/groups/<string:group_id>')
-api.add_resource(Group2, '/groups/<string:group_id>/users')
-api.add_resource(Group3, '/groups/<string:group_id>/users/<string:user_id>')
+
+api.add_resource(Group1, '/groups')
+api.add_resource(Group2, '/groups/<string:group_id>')
+api.add_resource(Group3, '/groups/<string:group_id>/users')
+api.add_resource(Group4, '/groups/<string:group_id>/users/<string:user_id>')
 api.add_resource(Users1, '/users')
 api.add_resource(Users2, '/users/<string:user_id>')
 api.add_resource(Users3, '/users/<string:user_id>/groups')
